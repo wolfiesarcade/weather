316baa5a5dce488baf11b7548e4d95aa6a031213
Elliott <etwilley@example.com>
2023-07-13 14:44:50 -0500

    faster loading

--- icons.py
+++ icons.py
@@ -1,12 +1,12 @@
 icon_dictionary = {
-    "01d": "https://www.dropbox.com/s/v5lqdkvfgdq89td/sun.png?raw=1",
-    "02d": "https://www.dropbox.com/s/qnjv086k35fvm9b/cloudy.png?raw=1",
-    "03d": "https://www.dropbox.com/s/a37cl9pb70b5c9i/clouds.png?raw=1",
-    "04d": "https://www.dropbox.com/s/a37cl9pb70b5c9i/clouds.png?raw=1",
-    "09d": "https://www.dropbox.com/s/ojlaaa9k0p34vot/rain.png?raw=1",
-    "10d": "https://www.dropbox.com/s/ojlaaa9k0p34vot/rain.png?raw=1",
-    "11d": "https://www.dropbox.com/s/wrlwpzbrq3i5uru/storm.png?raw=1",
-    "50d": "https://www.dropbox.com/s/p239n7xffx1g0aq/fog.png?raw=1",
+    "01d": "./static/sun.png",
+    "02d": "./static/cloudy.png",
+    "03d": "./static/clouds.png",
+    "04d": "./static/clouds.png",
+    "09d": "./static/rain.png",
+    "10d": "./static/rain.png",
+    "11d": "./static/storm.png",
+    "50d": "./static/fog.png",
 }
 
 
