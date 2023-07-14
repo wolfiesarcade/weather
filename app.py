ba20e37a1c442e1a3fe66a81ebebb3c8ca0480c1
wolfiesarcade <itoliverpeter2@gmail.com>
2023-07-14 13:53:07 -0400

    made changes to add a landing page and cleaned up code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import requests
 from icons import icons, icon_dictionary
-# from videos import video, video_dictionary
+from video import video, video_dictionary
 from flask import Flask, render_template, request
-# from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -10,39 +10,8 @@ app.config["DEBUG"] = True
 
 @app.route("/")
 def index():
-    api_key = "bc5ef4c4fdcb0080ee220ad05699e411"
     city = "New York"
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-    dateurl = f"http://worldtimeapi.org/api/timezone/America/New_York"
-    response = requests.get(url)
-    dateresponse = requests.get(dateurl)
-    date = dateresponse.json()
-
-    if response.status_code == 200:
-        data = response.json()
-        # Extract the relevant information from the response
-        temperature = data["main"]["temp"]
-        farh= int((temperature - 273.15) * 9 // 5 + 32)
-        celsius = int(temperature - 273.15)
-        humidity = data["main"]["humidity"]
-        description = data["weather"][0]["description"]
-        icondata = data["weather"][0]["icon"]
-        iconexecute=icons(icondata)
-        # videoexecute=video(icondata)
-
-        # Print the weather information
-        weather = {
-            "city": city.capitalize(),
-            "Temperature": temperature,
-            "Humidity": humidity,
-            "Description": description,
-            "icon": icondata,
-        }
-        print(weather)
-    else:
-        print("Error:", response.status_code)
-
-    return render_template("weather.html", weather=weather, celsius=celsius, farh=farh, temperature=temperature, iconexecute=iconexecute)
+    return render_template("lander.html")
 
 
 @app.route("/test", methods=['GET'])
@@ -66,7 +35,9 @@ def cityName():
             description = data["weather"][0]["description"]
             icondata = data["weather"][0]["icon"]
             iconexecute=icons(icondata)
-            # videoexecute=video(icondata)
+            videoexecute=video(icondata)
+            dt = data["dt"]
+            current_date = datetime.datetime.fromtimestamp(dt).date()
 
             # Print the weather information
             weather = {
@@ -75,10 +46,11 @@ def cityName():
                 "Humidity": humidity,
                 "Description": description,
                 "icon": icondata,
+                "Current Date": current_date
             }
             print(weather)
         else:
             print("Error:", response.status_code)
 
 
-        return render_template("weather.html", weather=weather, celsius=celsius, farh=farh, temperature=temperature, iconexecute=iconexecute, cityname=city)
+        return render_template("weather.html", weather=weather, celsius=celsius, farh=farh, temperature=temperature, iconexecute=iconexecute, cityname=city, videoexecute=videoexecute, current_date=current_date)
